6f6d0a4c115f2178414f4e03eac6130816c9e819
Lucas Neves <lucas54neves@gmail.com>
2021-06-25 19:25:28 -0300

    add handling error

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -7,44 +7,50 @@ import { MailDTO } from '../dtos/MailDTO'
 
 class UsersController {
   async sendMail(request: Request, response: Response): Promise<Response> {
-    const { name, email, password } = request.body
+    try {
+      const { name, email, password } = request.body
 
-    const user = {
-      name,
-      email,
-      password
-    }
-
-    const queue = new Queue<MailDTO>(config.mail.queueName, {
-      connection: config.redis.connection
-    })
-
-    await queue.add('send-simple', {
-      from: 'Queue Test <queue@queuetest.com.br>',
-      subject: 'Cadastro de usuário',
-      text: `Olá, ${user.name}, bem-vindo ao sistema de filas`,
-      to: `${user.name} <${user.email}>`
-    })
-
-    const worker = new Worker(config.mail.queueName, async (job: Job) => {
-      if (config.mail.smtp.host) {
-        const transporter = nodemailer.createTransport(config.mail.smtp)
-
-        transporter.sendMail(job.data)
+      const user = {
+        name,
+        email,
+        password
       }
-    })
 
-    worker.on('completed', (job) => {
-      console.log(
-        `Completed job ${job.id} successfully, sent email to ${job.data.to}`
+      const queue = new Queue<MailDTO>(config.mail.queueName, {
+        connection: config.redis.connection
+      })
+
+      await queue.add('send-simple', {
+        from: 'Queue Test <queue@queuetest.com.br>',
+        subject: 'Cadastro de usuário',
+        text: `Olá, ${user.name}, bem-vindo ao sistema de filas`,
+        to: `${user.name} <${user.email}>`
+      })
+
+      const worker = new Worker(config.mail.queueName, async (job: Job) => {
+        if (config.mail.smtp.host) {
+          const transporter = nodemailer.createTransport(config.mail.smtp)
+
+          transporter.sendMail(job.data)
+        }
+      })
+
+      worker.on('completed', (job) => {
+        console.log(
+          `Completed job ${job.id} successfully, sent email to ${job.data.to}`
+        )
+      })
+
+      worker.on('failed', (job: Job, err: any) =>
+        console.log(`Failed job ${job.id} with ${err}`)
       )
-    })
 
-    worker.on('failed', (job: Job, err: any) =>
-      console.log(`Failed job ${job.id} with ${err}`)
-    )
-
-    return response.status(201).send()
+      return response.status(201).send()
+    } catch (error) {
+      return response.status(400).json({
+        message: error.message
+      })
+    }
   }
 }
 
