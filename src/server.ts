585b74f448bae5dd4d71dd81cf3678780c0c94f1
Lucas Neves <lucas54neves@gmail.com>
2021-06-28 13:07:54 -0300

    add queue test

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { ExpressAdapter } from '@bull-board/express'
 import Redis from 'ioredis'
 import { Queue as QueueMQ, QueueScheduler, Worker } from 'bullmq'
 import nodemailer from 'nodemailer'
+import fetch from 'node-fetch'
 
 import config from './config'
 
@@ -13,8 +14,8 @@ const app = express()
 
 const redis = new Redis(process.env.REDIS_URL)
 
-function createQueueMQ() {
-  return new QueueMQ(config.mail.queueName, { connection: redis })
+function createQueueMQ(name: string) {
+  return new QueueMQ(name, { connection: redis })
 }
 
 async function setupBullMQProcessor() {
@@ -22,7 +23,13 @@ async function setupBullMQProcessor() {
     connection: redis
   })
 
+  const delayQueueScheduler = new QueueScheduler('delay', {
+    connection: redis
+  })
+
   await queueScheduler.waitUntilReady()
+
+  await delayQueueScheduler.waitUntilReady()
 
   new Worker(
     config.mail.queueName,
@@ -33,10 +40,28 @@ async function setupBullMQProcessor() {
     },
     { connection: redis }
   )
+
+  new Worker(
+    'delay',
+    async (job) => {
+      const responses = []
+
+      for (let i = 0; i < job.data.count; i++) {
+        responses.push(
+          await fetch(`http://httpbin.org/delay/${job.data.delay}`)
+        )
+      }
+
+      return responses
+    },
+    { connection: redis }
+  )
 }
 
 async function run() {
-  const queue = createQueueMQ()
+  const queue = createQueueMQ(config.mail.queueName)
+
+  const delayQueue = createQueueMQ('delay')
 
   await setupBullMQProcessor()
 
@@ -45,13 +70,24 @@ async function run() {
   serverAdapter.setBasePath('/admin/queues')
 
   createBullBoard({
-    queues: [new BullMQAdapter(queue)],
+    queues: [new BullMQAdapter(queue), new BullMQAdapter(delayQueue)],
     serverAdapter
   })
 
   app.use(express.json())
 
   app.use('/admin/queues', serverAdapter.getRouter())
+
+  app.use('/delay', async (request: Request, response: Response) => {
+    const { delay, count } = request.query
+
+    await delayQueue.add('delay-job', {
+      delay,
+      count
+    })
+
+    return response.status(201).send()
+  })
 
   app.use('/users', async (request: Request, response: Response) => {
     try {
